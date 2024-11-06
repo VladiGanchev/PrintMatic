1213752574c14114ef69dedf1854ed7426167e07
MimDel <mariadelikoceva@gmail.com>
2024-11-06 23:48:40 +0200

    first commit

---
+++ src/main/java/com/example/printmatic/PrintmaticApplication.java
@@ -0,0 +1,13 @@
+package com.example.printmatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintmaticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintmaticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/printmatic/PrintmaticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.printmatic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintmaticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
